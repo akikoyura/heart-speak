94c25eac268e0a83a700de884e9b8305a2f5fddb
HoangMH1 <hmaiknight@gmail.com>
2024-09-15 14:25:41 +0700

    update code for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://akikoyura.github.io/heart-speak/', // comment this line if you are not deploying to GitHub Pages
   plugins: [react()],
 })
